7767a63d9b901332a83d136e6212e90242942994
Klas Österman <klas-png@users.noreply.github.com>
2026-08-09 18:26:08 +0200

    Improve booking paths and show details

--- shows.js
+++ shows.js
@@ -5,76 +5,98 @@ window.SHOWS = [
     displayDate: "5 september 2026",
     title: "Dop",
     location: "Glanshammars kyrka",
-    act: "Klas Österman & Josefin Blomberg (duo)"
+    act: "Klas Österman & Josefin Blomberg (duo)",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-08-29",
     displayDate: "29 augusti 2026",
     title: "Frukostmingel vid golftävling",
     location: "Roslagens GK",
-    act: "Klas Österman & Josefin Blomberg (duo)"
+    act: "Klas Österman & Josefin Blomberg (duo)",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-08-15",
     displayDate: "15 augusti 2026",
     title: "Bröllop och vigsel",
     location: "Hille kyrka, Gävle",
-    act: "Klas Österman & Josefin Blomberg (duo)"
+    act: "Klas Österman & Josefin Blomberg (duo)",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-08-08",
     displayDate: "8 augusti 2026",
     title: "Bröllop, mingel och vigsel",
     location: "Nyckelviken",
-    act: "Klas Österman & Josefin Blomberg (duo)"
+    act: "Klas Österman & Josefin Blomberg (duo)",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-07-18",
     displayDate: "18 juli 2026",
     title: "Födelsedagsfest",
     location: "Rimbo",
-    act: "Klas Österman & Josefin Blomberg (duo)"
+    act: "Klas Österman & Josefin Blomberg (duo)",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-07-16",
     displayDate: "16 juli 2026",
     title: "Live",
     location: "Ingmarsö Krog",
-    act: "Klas Österman & Josefin Blomberg"
+    act: "Klas Österman & Josefin Blomberg",
+    visibility: "public",
+    format: "Duo"
   },
   {
     date: "2026-07-11",
     displayDate: "11 juli 2026",
     title: "Live med band",
     location: "Logen, Östra Lagnö",
-    act: "Klas Österman & Josefin Blomberg + band"
+    act: "Klas Österman & Josefin Blomberg + band",
+    visibility: "public",
+    format: "Fullt band"
   },
   {
     date: "2026-06-27",
     displayDate: "27 juni 2026",
     title: "Privat bröllop",
     location: "Idöborg, Värmdö",
-    act: "Klas Österman & Josefin Blomberg"
+    act: "Klas Österman & Josefin Blomberg",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-06-26",
     displayDate: "26 juni 2026",
     title: "Live",
     location: "Café Chocolat, Rimbo",
-    act: "Klas Österman & Josefin Blomberg"
+    act: "Klas Österman & Josefin Blomberg",
+    visibility: "public",
+    format: "Duo"
   },
   {
     date: "2026-06-13",
     displayDate: "13 juni 2026",
     title: "Privat bröllop",
     location: "Fredriksborgs Hotell, Värmdö",
-    act: "Klas Österman & Josefin Blomberg"
+    act: "Klas Österman & Josefin Blomberg",
+    visibility: "private",
+    format: "Duo"
   },
   {
     date: "2026-06-11",
     displayDate: "11 juni 2026",
     title: "Privat bröllop",
     location: "Rimbo kyrka",
-    act: "Klas Österman & Josefin Blomberg"
+    act: "Klas Österman & Josefin Blomberg",
+    visibility: "private",
+    format: "Duo"
   }
 ];
